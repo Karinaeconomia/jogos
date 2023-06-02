043599052e063728e53425dbad3c659dc9364da0
Karinaeconomia <karinacarvalhodinizferreira@gmail.com>
2023-06-01 21:24:44 -0300

    teste

---
+++ forca.py
@@ -0,0 +1,17 @@
+
+
+def jogar():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("Fim de jogo")
+
+if(__name__=="__manin__"):
+    jogar()
+
+
+
+
+
+
